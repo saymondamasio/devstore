57c680b80ff184080dca9e9de094a6c8ea830368
Saymon Damásio <saymon.damasio95@gmail.com>
2023-12-06 20:40:23 -0300

    feat: create routes api

---
+++ src/data/api.ts
@@ -0,0 +1,9 @@
+import { env } from '~/env'
+
+export async function api(path: string, init?: RequestInit) {
+	const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+
+	const url = new URL(path, baseUrl)
+
+	return await fetch(url, init)
+}
